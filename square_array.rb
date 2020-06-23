4b7a5b7449e5825417615961bdef87106b489b3e
Charlie Barth <charliepbarth@gmail.com>
2020-06-23 19:38:18 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,10 @@
 def square_array(array)
   
-  # array_with_nums = ["#{array}"]
-  
   array.each do |num|
+  array_with_nums[]
   array_with_nums.push(num * num)
   
-end
+  end
 end
 
 array_two = [1, 2, 3]
